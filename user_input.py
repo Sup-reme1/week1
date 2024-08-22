0f63d96a335be3ec51ce5ae137df323cd0975b78
Sup-reme1 <abdulbasitolalekan20@gmail.com>
2024-08-22 12:10:45 +0100

    Python Assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('Enter your name: ')
+location = input('Enter your location: ')
+age = input('Enter your age: ')
+
+print('Hello ', name, 'you are ', age, 'years old and live in ', location, '.')
